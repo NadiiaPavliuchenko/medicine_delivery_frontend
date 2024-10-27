a5b66dd262f2553664172f7a60929955e9a75579
NadiiaPavliuchenko <n.pavljuchenko@gmail.com>
2024-10-27 16:31:22 +0200

    Updates

---
+++ static/js/262.c503e033.chunk.js
@@ -0,0 +1,44 @@
+"use strict";(self.webpackChunkmedicine_delivery_frontend=self.webpackChunkmedicine_delivery_frontend||[]).push([[262],{2262:(e,n,t)=>{t.r(n),t.d(n,{default:()=>p});t(5043);var i=t(3216),r=t(197),d=t(5475);const o=r.Ay.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  height: 60px;
+  padding: 0 20px;
+  margin-bottom: 5px;
+  @media screen and (max-width: 767px) {
+    justify-content: center;
+  }
+`,a=r.Ay.nav`
+  display: flex;
+  gap: 20px;
+  list-style: none;
+  margin: 0;
+`,s=r.Ay.li`
+  &::after {
+    content: "";
+    position: absolute;
+    left: 120px;
+    top: 16px;
+    width: 1px;
+    height: 30px;
+    background-color: #333;
+    @media screen and (min-width: 768px) {
+      left: 92px;
+    }
+  }
+`,c=(0,r.Ay)(d.k2)`
+  text-decoration: none;
+  color: #333;
+  padding: 8px 12px;
+
+  &:hover,
+  &:focus {
+    text-decoration: underline;
+  }
+
+  &.active {
+    text-decoration: underline;
+  }
+`;var l=t(579);const p=()=>(0,l.jsxs)(l.Fragment,{children:[(0,l.jsx)(o,{children:(0,l.jsxs)(a,{children:[(0,l.jsx)(s,{children:(0,l.jsx)(c,{to:"/",children:"Shop"})}),(0,l.jsx)("li",{children:(0,l.jsx)(c,{to:"/cart",children:"Shophing Cart"})})]})}),(0,l.jsx)(i.sv,{})]})}}]);
+//# sourceMappingURL=262.c503e033.chunk.js.map
