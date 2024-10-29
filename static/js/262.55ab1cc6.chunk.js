f989537745291df5b840e4915e74faae34789c13
NadiiaPavliuchenko <72461487+NadiiaPavliuchenko@users.noreply.github.com>
2024-10-29 18:36:24 +0000

    Deploying to gh-pages from @ NadiiaPavliuchenko/medicine_delivery_frontend@d3237ab6fa043be162098b5a76672502d18e6f73 🚀

---
+++ static/js/262.55ab1cc6.chunk.js
@@ -0,0 +1,36 @@
+"use strict";(self.webpackChunkmedicine_delivery_frontend=self.webpackChunkmedicine_delivery_frontend||[]).push([[262],{2262:(e,n,i)=>{i.r(n),i.d(n,{default:()=>x});i(5043);var t=i(3216),r=i(197),d=i(5475);const s=r.Ay.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  height: 60px;
+  padding: 0 20px;
+  margin-bottom: 5px;
+  @media screen and (max-width: 767px) {
+    justify-content: center;
+  }
+`,a=r.Ay.nav`
+  display: flex;
+  gap: 20px;
+  list-style: none;
+  margin: 0;
+`,c=r.Ay.span`
+  display: block;
+  width: 1px;
+  height: 30px;
+  background-color: #333;
+`,o=(0,r.Ay)(d.k2)`
+  text-decoration: none;
+  color: #333;
+  padding: 8px 12px;
+
+  &:hover,
+  &:focus {
+    text-decoration: underline;
+  }
+
+  &.active {
+    text-decoration: underline;
+  }
+`;var l=i(579);const x=()=>(0,l.jsxs)(l.Fragment,{children:[(0,l.jsx)(s,{children:(0,l.jsxs)(a,{children:[(0,l.jsx)("li",{children:(0,l.jsx)(o,{to:"/",children:"Shop"})}),(0,l.jsx)(c,{}),(0,l.jsx)("li",{children:(0,l.jsx)(o,{to:"/cart",children:"Shophing Cart"})})]})}),(0,l.jsx)(t.sv,{})]})}}]);
+//# sourceMappingURL=262.55ab1cc6.chunk.js.map
